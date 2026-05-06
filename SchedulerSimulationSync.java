5480e68ab36c95c488e5e5b79e119b59a04bd7a8
hanen-aldosri-445 <445052141@std.psau.edu.sa>
2026-05-06 04:51:15 +0300

    adding waiting time

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -80,7 +80,13 @@ class SharedResources {
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+      
+        waitingTimeLock.lock(); 
+        try { 
+            totalWaitingTime += time; 
+        } finally { 
+            waitingTimeLock.unlock(); 
+        }
     }
     
     // Method to log execution
